560cf84d8b9700692b15e689f8d5558c478c1787
dicksontan <dicksontan2618@gmail.com>
2023-05-15 20:44:33 +0800

    finalize q1 code and comments

--- RadixSortQ1.java
+++ RadixSortQ1.java
@@ -1,14 +1,16 @@
-package CPT212_Asg1;
-
-// import ArrayList and Arrays
+// to create array and resizable array (ArrayList)
 import java.util.ArrayList;
 import java.util.Arrays;
+
+// to get user input
 import java.util.Scanner;
+
+// to generate random values
 import java.util.concurrent.ThreadLocalRandom;
 
-public class RadixSort {
+public class RadixSortQ1 {
 
-    // function to get the largest number in the input array so that we can obtain the largest digit/significant number 
+    // function to get the largest number in the input array so that we can obtain the largest significant figure
     private static int getMax(int[] input_arr) {
         int max = input_arr[0];
         for (int i = 1; i < input_arr.length; i++) {
@@ -19,7 +21,7 @@ public class RadixSort {
         return max;
     }
 
-    // function to sort the number into their respective
+    // function to sort the number ascendingly from 0-9 based on their significant figures
     private static void sortNumber(int[] input_arr, int place, ArrayList<Integer>[] result, ArrayList<Integer> store) {
 
         // get input array length
@@ -69,6 +71,7 @@ public class RadixSort {
     }
 
     // call function
+    // "arr" is the original array that passed into this function when called in "main"
     public static void radixSort(int[] arr) {
 
         // check if input array is empty
@@ -76,7 +79,7 @@ public class RadixSort {
             return;
         }
 
-        // call "getMax" to get maximum number to obtain the largest digit/significant number in input array
+        // call "getMax" to get maximum number to obtain the significant figure in input array
         int max = getMax(arr);
 
         // declare "array1" and "array2" for sorting purposes
@@ -93,10 +96,12 @@ public class RadixSort {
             array2[i] = new ArrayList<Integer>();
         }
 
-        // "count" is used to determine which array (array1/array2) is being used at that particular significant number
+        // "count" is used to determine which array (array1/array2) is being used at that particular significant figure
         int count = 0;
-        // for loop to increase the decimal place/significant number for each iteration 
+        // for loop to increase the decimal place/significant figure for each iteration
         for (int place = 1; max / place > 0; place *= 10) {
+            // the print functions is to visualize the state of "array1" and "array2" at each iteration
+            // "sortNumber" is called for each iteration to sort the digits ascendingly for each significant figure
             if (count % 2 == 0) {
                 System.out.println("Using Array 1!");
                 sortNumber(arr, place, array1, store);
@@ -118,33 +123,29 @@ public class RadixSort {
 
     public static void main(String[] args) {
 
-        // initialize input data
-        // int[] data = { 275, 87, 426, 61, 409, 17012, 677, 503 };
-
-        // // data passed to fucntion to be sorted
-        // RadixSort.radixSort(data);
-
-        // System.out.println("");
-        // System.out.println("Sorted List: ");
-        // System.out.print(Arrays.toString(data));
+        // initialize  original array
         int data[];
+
+        // initialize variables that use to get input from user later
         String temp;
-        int arrSize=1;
+        int arrSize = 1;
+
+        // to get input from user on the size of original array
+        // prompt user to input again if the input is not a number
         try (Scanner myObj = new Scanner(System.in)) {
-            boolean containsOnlyDigits=false;
-            while (containsOnlyDigits==false){
-                containsOnlyDigits=true;
-            System.out.println("Enter array size : ");
-            temp = myObj.nextLine();
+            boolean containsOnlyDigits = false;
+            while (containsOnlyDigits == false) {
+                containsOnlyDigits = true;
+                System.out.println("Enter array size : ");
+                temp = myObj.nextLine();
                 for (int i = 0; i < temp.length(); i++) {
-                    if (!Character.isDigit(temp.charAt(i))) { // in case that a char is NOT a digit, enter to the if code block
-                            containsOnlyDigits = false;
+                    if (!Character.isDigit(temp.charAt(i))) { 
+                        containsOnlyDigits = false;
                     }
                 }
 
                 if (containsOnlyDigits) {
-                    arrSize=Integer.parseInt(temp);
-                    //  System.out.println("Array size is "+ arrSize);
+                    arrSize = Integer.parseInt(temp);
                 } else {
                     System.out.println("Please input a number!");
                     System.out.println();
@@ -152,19 +153,27 @@ public class RadixSort {
             }
         } catch (NumberFormatException e) {
             e.printStackTrace();
-        } 
-        data = new int[arrSize];
-        for (int i=0; i<arrSize;i++){
-            data[i]=ThreadLocalRandom.current().nextInt(1, 100000 + 1);
         }
+
+        // initialize the size of original array based on input size
+        data = new int[arrSize];
+
+        // add random numbers into original array
+        for (int i = 0; i < arrSize; i++) {
+            data[i] = ThreadLocalRandom.current().nextInt(1, 100000 + 1);
+        }
+
+        // show original array
         System.out.println("");
         System.out.println("Original List: ");
         System.out.print(Arrays.toString(data));
         System.out.println("");
-        
-        System.out.println("");
-        RadixSort.radixSort(data);
 
+        // pass original array to radix sort algorithm
+        System.out.println("");
+        RadixSortQ1.radixSort(data);
+
+        // show sorted array
         System.out.println("");
         System.out.println("Sorted List: ");
         System.out.print(Arrays.toString(data));
